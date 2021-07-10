86efb759f49260f34e448c9f7fae5c658e37857e
Nikita Mozhaev <nikita.s.mozhaev@gmail.com>
2021-07-10 16:23:57 +0300

    Initial commit

---
+++ app/src/main/java/com/fruits/carnival/Web.java
@@ -0,0 +1,45 @@
+package com.fruits.carnival;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Web extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+
+        web = findViewById(R.id.web);
+        web.loadUrl("http://whiteleads.site/HRMGXBF9");
+        web.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = web.getSettings();
+        CookieManager.getInstance().setAcceptCookie(true);
+        webSettings.setJavaScriptEnabled(true);
+
+        //new
+        webSettings.setAllowContentAccess(true);
+        webSettings.setBlockNetworkLoads(false);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setDomStorageEnabled(true);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (web.canGoBack()){
+            web.goBack();
+        }else{
+            super.onBackPressed();
+        }
+
+    }
+}
